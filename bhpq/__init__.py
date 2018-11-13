2a841931fee8d3a19bddd8c4d2076be33e4affb9
aayushuppal <aayuppal@gmail.com>
2018-11-13 01:59:11 -0500

    bump version to 1.0.4

--- bhpq/__init__.py
+++ bhpq/__init__.py
@@ -6,6 +6,6 @@ See https://github.com/aayushuppal/bhpq for more information
 """
 
 # Version of bhrq package
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 from .bhpq import BinaryHeapPriorityQueue
